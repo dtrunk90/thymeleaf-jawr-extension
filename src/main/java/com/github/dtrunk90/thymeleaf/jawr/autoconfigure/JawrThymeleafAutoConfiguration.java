254ab5457f850a9b83b777f149e272c48d02bba9
Danny Trunk <dtrunk90@gmail.com>
2016-04-16 15:14:43 +0200

    spring boot autoconfiguration: create dialect instance only if missing

--- src/main/java/com/github/dtrunk90/thymeleaf/jawr/autoconfigure/JawrThymeleafAutoConfiguration.java
+++ src/main/java/com/github/dtrunk90/thymeleaf/jawr/autoconfigure/JawrThymeleafAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.github.dtrunk90.thymeleaf.jawr.autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import com.github.dtrunk90.thymeleaf.jawr.dialect.JawrDialect;
 @AutoConfigureAfter(WebMvcAutoConfiguration.class)
 public class JawrThymeleafAutoConfiguration {
 	@Bean
+	@ConditionalOnMissingBean
 	public JawrDialect jawrDialect() {
 		return new JawrDialect();
 	}
